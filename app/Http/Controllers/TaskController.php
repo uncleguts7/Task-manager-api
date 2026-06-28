ea47d62f2b878ad6b0fc27c448c28c44445cc4e0
guts <uncleguts7@gmail.com>
2026-06-28 17:13:32 +0300

    feat: Task manager REST API - basic CRUD

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = Task::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'nullable|string',
+            'due_date' => 'required|date',
+            'status' => 'nullable|in:pending,in_progress,completed',
+        ]);
+
+        $data = Task::create($request->all());
+
+        return response()->json($data,201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Task $task)
+    {
+        return response()->json($task);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Task $task)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'name' => 'sometimes|required|string',
+            'description' => 'sometimes|nullable|string',
+            'due_date' => 'sometimes|required|date',
+            'status' => 'nullable|in:pending,in_progress,completed',
+        ]);
+
+        $task->update($request->all());
+
+        return response()->json($task, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Task $task)
+    {
+        $task->delete();
+
+        return response()->json(null, 204);
+    }
+}
